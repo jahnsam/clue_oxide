4738b98bc7a457c8f18a0ed86607a8f0dedf3b56
kudarizaka <jahn.samuel@gmail.com>
2022-12-07 02:59:34 -0800

    lexer tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,6 @@
+use crate::clue_errors::*;
+use substring::Substring;
+
 struct Lexer{
   input: String,
   position: usize,
@@ -6,33 +9,41 @@ struct Lexer{
 
 impl Lexer{
   fn new(filename: &str) -> Result<Self,CluEError>{
-    if let Ok(input) = read_file(filename){
+    if let Ok(input) = std::fs::read_to_string(filename){
       return Ok(Lexer{input, position: 0, line_number: 1});
     }
-    Err(CluEError::InvalidConfigFile(filename.clone()))
-  };
+    Err(CluEError::InvalidConfigFile(filename.to_string()))
+  }
   //----------------------------------------------------------------------------
   fn next_token(&mut self) -> Token {
 
     let idx = self.position;
     
-    for read_to in idx..self.input.len() {
+    if let Some(token) = identify_token(self.input.substring(idx,idx+1)){
+      self.position += 1;
+      return token;
+    }
 
-      if !is_token_over(self.input[read_to]){ continue; }
+    for read_to in idx+1..self.input.len() {
+
+      if !is_token_over(self.input.substring(read_to,read_to+1)){ continue; }
         
       self.position = read_to;
 
-      if let Some(token) = identify_token(self.input[idx..=read_to]){
+      if let Some(token) 
+        = identify_token(self.input.substring(idx,read_to) ){
         return token;
       }
 
-      return Token::UserInputValue( Box(self.input[idx .. read_to].clone()) );
+      return Token::UserInputValue( 
+          Box::new(self.input.substring(idx, read_to).to_string()) );
       
     }
 
     self.position = self.input.len();
 
-    Token::UserInputValue(Box(self.input[idx ..].clone()))
+    Token::UserInputValue(Box::new(
+          self.input.substring(idx,self.input.len() ).to_string()))
 
   }
 
@@ -49,24 +60,28 @@ fn is_token_over(word: &str) -> bool{
   }
 }
 //------------------------------------------------------------------------------
-fn parse_token(filename: &str){
+fn parse_tokens(mut lexer: Lexer) -> Result<Vec::<Token>, CluEError>{
 
-  let mut lexer = Lexer::new(filename);
+  //let mut lexer = Lexer::new(filename)?;
 
   let mut tokens = Vec::<Token>::with_capacity(lexer.input.len());
 
   while lexer.position < lexer.input.len(){
-    if let Ok(token) = lexer.next_token(){
-      if token == Token::EOL {
-        lexer.line_number += 1;
-      }
-      tokens.push(token)
+    
+    let token = lexer.next_token();
+    
+    if token == Token::EOL {
+      lexer.line_number += 1;
     }
+
+    tokens.push(token)
+    
   }
+  Ok(tokens)
 }
 
 //------------------------------------------------------------------------------
-fn find_comments(in_tokens: Vec::<Token>) -> Result<Vec::<Token>,CluEError> {
+fn find_comments(in_tokens: Vec::<Token>) -> Vec::<Token> {
 
   let mut out_tokens = Vec::<Token>::with_capacity(in_tokens.len());
   
@@ -86,29 +101,29 @@ fn find_comments(in_tokens: Vec::<Token>) -> Result<Vec::<Token>,CluEError> {
       }
     }
 
-    if !was_last_token_slash && !was_last_token_times{
+    if !was_last_token_slash && !was_last_token_times{ // R'R
       out_tokens.push(token);
     }
-    else if was_last_token_slash && token == Token::Slash{
+    else if was_last_token_slash && token == Token::Slash{ // //
       was_last_token_slash = false;
       out_tokens.push(Token::LineComment);
     }
-    else if was_last_token_slash && token == Token::Times
+    else if was_last_token_slash && token == Token::Times { // /*
       was_last_token_slash = false;
       out_tokens.push(Token::BlockCommentStart);
     }
-    else if was_last_token_times && token == Token::Slash{
+    else if was_last_token_times && token == Token::Slash{ // */
       was_last_token_times = true;
       out_tokens.push(Token::BlockCommentEnd);
     }
-    else if was_last_token_slash{
+    else if was_last_token_slash{ // /R
       was_last_token_slash = false;
       out_tokens.push(Token::Slash);
       out_tokens.push(token);
     }
-    else if was_last_token_times{
+    else if was_last_token_times{ // *R
       was_last_token_times = true;
-      out_tokens.push(Token::times);
+      out_tokens.push(Token::Times);
       out_tokens.push(token);
     }
 
@@ -135,7 +150,7 @@ fn prune_tokens(in_tokens: Vec::<Token>) -> Result<Vec::<Token>,CluEError> {
       line_number += 1;
       if is_attribute{
         is_attribute = false;
-        tokens.push(Token::Semicolon);
+        out_tokens.push(Token::Semicolon);
       }
       continue;
     }
@@ -158,7 +173,7 @@ fn prune_tokens(in_tokens: Vec::<Token>) -> Result<Vec::<Token>,CluEError> {
     
     }
 
-    if token == Token::WhiteSpace{ continue; }
+    if token == Token::Whitespace{ continue; }
       
     if token == Token::Sharp{is_attribute = true;}
     out_tokens.push(token);
@@ -168,8 +183,9 @@ fn prune_tokens(in_tokens: Vec::<Token>) -> Result<Vec::<Token>,CluEError> {
   Ok(out_tokens)
 }
 //------------------------------------------------------------------------------
+#[derive(PartialEq, Debug, Clone)]
 enum Token{
- BlockCommetEnd, 
+ BlockCommentEnd, 
  BlockCommentStart, 
  Coma,
  CurlyBracketClose,
@@ -190,11 +206,11 @@ enum Token{
  Times,
  TunnelSplitting,
  UserInputValue(Box<String>),
- WhiteSpace,
+ Whitespace,
 }
 
 //------------------------------------------------------------------------------
-fn identify_token(word &str) -> Option<Token>{
+fn identify_token(word: &str) -> Option<Token>{
   match word{
     //"*/" => Some(Token::BlockCommetEnd),
     //"/*" => Some(Token::BlockCommetStart),
@@ -219,23 +235,23 @@ fn identify_token(word &str) -> Option<Token>{
     " " => Some(Token::Whitespace),
     _ => None
   }
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>}
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 struct TokenStatements{
   statements: Vec::<Vec::<Token>>,
-  line_number: Vec::<usize>,
+  line_numbers: Vec::<usize>,
 }
 
 fn get_token_statements(tokens: Vec::<Token>) -> TokenStatements {
-  Vec::<Vec::<Token>>
 
     // Count statements.
     let mut num_statements = 0;
 
     for token in tokens.iter(){
-      if token == Token::Semicolon { 
+      if *token == Token::Semicolon { 
         num_statements += 1;
       }
     }
@@ -243,6 +259,7 @@ fn get_token_statements(tokens: Vec::<Token>) -> TokenStatements {
 
     // Get Statements.
     let mut statements = Vec::<Vec::<Token>>::with_capacity(num_statements);
+    let mut line_numbers = Vec::<usize>::with_capacity(num_statements);
 
     for _istatement in 0..num_statements{
     
@@ -250,7 +267,7 @@ fn get_token_statements(tokens: Vec::<Token>) -> TokenStatements {
       let mut num_tokens_in_statement = 0;
       
       for token in tokens.iter(){
-        if token == Token::Semicolon { break;}
+        if *token == Token::Semicolon { break;}
         num_tokens_in_statement += 1;
       }
 
@@ -259,17 +276,101 @@ fn get_token_statements(tokens: Vec::<Token>) -> TokenStatements {
       let mut statement = Vec::<Token>::with_capacity(num_tokens_in_statement);
 
       for token in tokens.iter(){
-        if token == Token::Semicolon { break;}
-        statement.push(token);
+        if *token == Token::Semicolon { break;}
+        statement.push(token.clone());
       }
 
       statements.push(statement);
     } 
 
-    statements
+    TokenStatements{ statements, line_numbers}
 }
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
 
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#[cfg(test)]
+mod tests{
+  use super::*;
 
+  #[test]
+  fn test_identify_token(){
+  
+    assert_eq!(identify_token("element").unwrap(), Token::Element);
+    assert_eq!(identify_token(" ").unwrap(), Token::Whitespace);
+    assert_eq!(identify_token("in").unwrap(), Token::In);
+    assert_eq!(identify_token("[").unwrap(), Token::SquareBracketOpen);
+    assert_eq!(identify_token("]").unwrap(), Token::SquareBracketClose);
+    assert_eq!(identify_token(";").unwrap(), Token::Semicolon);
+    assert_eq!(identify_token("\n").unwrap(), Token::EOL);
+  }
+  //----------------------------------------------------------------------------
+
+  #[test]
+  fn test_parse_tokens(){
+  
+    let lexer = Lexer{
+      input: String::from(
+"element in [H];
+tunnel_splitting = 80e3; // Hz"),
+      position: 0, 
+      line_number: 0,
+    };
+
+    let ref_tokens = vec![Token::Element, Token::Whitespace, Token::In,
+     Token::Whitespace, Token::SquareBracketOpen, 
+     Token::UserInputValue(Box::new("H".to_string())), 
+     Token::SquareBracketClose, Token::Semicolon, Token::EOL, 
+     Token::TunnelSplitting, Token::Whitespace,
+     Token::Equals, Token::Whitespace, 
+     Token::UserInputValue(Box::new("80e3".to_string())), Token::Semicolon,
+     Token::Whitespace, Token::Slash, Token::Slash, Token::Whitespace,
+     Token::UserInputValue(Box::new("Hz".to_string())) ];
+
+
+    let tokens = parse_tokens(lexer).unwrap();
+
+    assert_eq!(tokens.len(), ref_tokens.len());
+   
+    for ii in 0..tokens.len(){
+      assert_eq!(tokens[ii], ref_tokens[ii]);
+    }
+
+  }
+  //----------------------------------------------------------------------------
+  #[test]
+  fn test_find_comments(){
+    let lexer = Lexer{
+      input: String::from(
+"/*element in [H];*/
+tunnel_splitting = 80e3; // Hz"),
+      position: 0, 
+      line_number: 0,
+    };
+    let ref_tokens = vec![ Token::BlockCommentStart,
+     Token::Element, Token::Whitespace, Token::In,
+     Token::Whitespace, Token::SquareBracketOpen, 
+     Token::UserInputValue(Box::new("H".to_string())), 
+     Token::SquareBracketClose, Token::Semicolon, 
+     Token::BlockCommentEnd, Token::EOL, 
+     Token::TunnelSplitting, Token::Whitespace,
+     Token::Equals, Token::Whitespace, 
+     Token::UserInputValue(Box::new("80e3".to_string())), Token::Semicolon,
+     Token::Whitespace, Token::LineComment, Token::Whitespace,
+     Token::UserInputValue(Box::new("Hz".to_string())) ];
+
+    let tokens = parse_tokens(lexer).unwrap();
+    let tokens = find_comments(tokens);
+
+
+    assert_eq!(tokens.len(), ref_tokens.len());
+   
+    for ii in 0..tokens.len(){
+      assert_eq!(tokens[ii], ref_tokens[ii]);
+    }
+
+
+  }
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
