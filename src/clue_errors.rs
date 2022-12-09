785ae368fb565f7c458985f0b4645ef30e905c27
kudarizaka <jahn.samuel@gmail.com>
2022-12-08 21:08:36 -0800

    more lexer logic

--- src/clue_errors.rs
+++ src/clue_errors.rs
@@ -1,16 +1,23 @@
 use std::fmt;
 
-#[derive(Debug,Clone)]
+#[derive(PartialEq,Debug,Clone)]
 pub enum CluEError{
+  CannotConvertToFloat(usize,String),
   InvalidConfigFile(String),
   //InvalidToken(usize,String),
   NoCentralSpinCoor,
+  NoRelationalOperators(usize),
+  NotAnOperator(usize,String),
   UnmatchedBlockComment(usize),
+  TooManyRelationalOperators(usize),
 }
 
 impl fmt::Display for CluEError{
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     match self{
+      CluEError::CannotConvertToFloat(line_number, token) => write!(f,
+          "{}: cannot convert \"{}\" to type float", line_number,token),
+
       CluEError::InvalidConfigFile(filename) => write!(f,
           "cannot not read config file \"{}\"", filename),
 
@@ -19,9 +26,20 @@ impl fmt::Display for CluEError{
 
       CluEError::NoCentralSpinCoor => write!(f,
           "coordinates for the detected spin were not defined"),
+      
+      CluEError::NoRelationalOperators(line_number) => write!(f,
+          "{}: no relational operators (=, <, >, in, ...), are present", 
+          line_number),
+
+      CluEError::NotAnOperator(line_number, token) => write!(f,
+          "{}: cannot interpret \"{}\" as an operator", line_number,token),
 
       CluEError::UnmatchedBlockComment(line_number) => write!(f,
           "{}: unmatched \"*/\"", line_number),
+
+      CluEError::TooManyRelationalOperators(line_number) => write!(f,
+          "{}: too many relational operators (=, <, >, in, ...), are present", 
+          line_number),
     }
   }
 }
--- src/lexer.rs
+++ src/lexer.rs
@@ -58,14 +58,14 @@ impl Lexer{
       }
 
       return Token::UserInputValue( 
-          Box::new(self.input.substring(idx, read_to).to_string()) );
+          self.input.substring(idx, read_to).to_string() );
       
     }
 
     self.position = self.input.len();
 
-    Token::UserInputValue(Box::new(
-          self.input.substring(idx,self.input.len() ).to_string()))
+    Token::UserInputValue(
+        self.input.substring(idx,self.input.len() ).to_string())
 
   }
 
@@ -219,17 +219,21 @@ fn prune_tokens(in_tokens: Vec::<Token>)
 pub enum Token{
  BlockCommentEnd, 
  BlockCommentStart, 
- Coma,
+ Comma,
  CurlyBracketClose,
  CurlyBracketOpen,
  Element,
  EOL,
  Equals,
+ Float(f64),
  In,
+ Int(i32),
  LineComment, 
  MagneticField,
+ Minus,
  ParenthesisClose,
  ParenthesisOpen,
+ Plus,
  Residue,
  Semicolon,
  Sharp,
@@ -238,16 +242,49 @@ pub enum Token{
  SquareBracketOpen,
  Times,
  TunnelSplitting,
- UserInputValue(Box<String>),
+ UserInputValue(String),
  Whitespace,
 }
+impl Token{
+  pub fn to_string(&self) -> String{
+    match self{
+      Token::BlockCommentEnd => "*/".to_string(), 
+      Token::BlockCommentStart => "/*".to_string(), 
+      Token::Comma => ",".to_string(),
+      Token::CurlyBracketClose => "}".to_string(),
+      Token::CurlyBracketOpen => "{".to_string(),
+      Token::Element => "element".to_string(),
+      Token::EOL => "\n".to_string(),
+      Token::Equals => "=".to_string(),
+      Token::Float(x) => x.to_string(),
+      Token::In => "in".to_string(),
+      Token::Int(n) => n.to_string(),
+      Token::LineComment => "//".to_string(), 
+      Token::MagneticField => "magnetic_field".to_string(),
+      Token::Minus => "-".to_string(),
+      Token::ParenthesisClose => ")".to_string(),
+      Token::ParenthesisOpen => "(".to_string(),
+      Token::Plus => "+".to_string(),
+      Token::Residue => "residue".to_string(),
+      Token::Semicolon => ";".to_string(),
+      Token::Sharp => "#".to_string(),
+      Token::Slash => "/".to_string(),
+      Token::SquareBracketClose => "]".to_string(),
+      Token::SquareBracketOpen => "[".to_string(),
+      Token::Times => "*".to_string(),
+      Token::TunnelSplitting => "tunnel_splitting".to_string(),
+      Token::UserInputValue(string) => (*string).clone(),
+      Token::Whitespace => " ".to_string(),
 
+    }
+  }
+}
 //------------------------------------------------------------------------------
 fn identify_token(word: &str) -> Option<Token>{
   match word{
     "*/" => Some(Token::BlockCommentEnd),
     "/*" => Some(Token::BlockCommentStart),
-    "," => Some(Token::Coma),
+    "," => Some(Token::Comma),
     "}" => Some(Token::CurlyBracketClose),
     "{" => Some(Token::CurlyBracketOpen),
     "element" => Some(Token::Element),
@@ -256,8 +293,10 @@ fn identify_token(word: &str) -> Option<Token>{
     "in" => Some(Token::In),
     "//" => Some(Token::LineComment),
     "magnetic_field" => Some(Token::MagneticField),
+    "-" => Some(Token::Minus),
     ")" => Some(Token::ParenthesisClose),
     "(" => Some(Token::ParenthesisOpen),
+    "+" => Some(Token::Plus),
     "residue" => Some(Token::Residue),
     ";" => Some(Token::Semicolon),
     "#" => Some(Token::Sharp),
@@ -272,6 +311,96 @@ fn identify_token(word: &str) -> Option<Token>{
 }
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+fn find_lhs_rhs_delimiter_index(tokens: &[Token], line_number: usize) 
+  -> Result<usize,CluEError>{
+  let mut found_token = false;
+  let mut index: usize = 0;
+
+  for ii in 0..tokens.len(){
+    if tokens[ii] == Token::Equals || tokens[ii] == Token::In{
+      if !found_token{
+        found_token = true;
+        index = ii;
+      }else{
+        return Err(CluEError::TooManyRelationalOperators(line_number));
+      }
+    }
+  }
+  if found_token{
+    return Ok(index);
+  }else{
+    return Err(CluEError::NoRelationalOperators(line_number));
+  }
+}
+//------------------------------------------------------------------------------
+struct TokenExpression{
+  lhs: Vec::<Token>,
+  rhs: Vec::<Token>,
+  relationship: Token
+}
+impl TokenExpression{
+  fn from(tokens: Vec::<Token>, line_number:usize) -> Result<Self,CluEError>{
+
+    let idx = find_lhs_rhs_delimiter_index(&tokens, line_number)?;
+
+    let mut lhs = Vec::<Token>::with_capacity(idx);
+    let mut rhs = Vec::<Token>::with_capacity(tokens.len() - idx - 1);
+    let relationship = tokens[idx].clone();
+
+    for (ii,token) in tokens.iter().enumerate(){
+
+      if ii < idx{
+        lhs.push(token.clone());
+      }else if ii > idx{
+        rhs.push(token.clone());
+      }
+    }
+    Ok(TokenExpression{lhs, rhs, relationship})
+
+  }
+}
+//------------------------------------------------------------------------------
+
+fn is_numeric(token: &Token) -> bool{
+  match token{
+    Token::Float(_x) => true,
+    Token::Int(_n) => true,
+    _ => false
+
+  }
+}
+//------------------------------------------------------------------------------
+fn basic_token_algebraic_operations_f64(tokens: &[Token;3],line_number: usize) 
+ ->Result<Token,CluEError>{
+
+  let a: f64;
+  match tokens[0]{
+    Token::Float(x) => a = x,
+    Token::Int(n) => a = n as f64,
+    _ => return 
+      Err(CluEError::CannotConvertToFloat(line_number, tokens[0].to_string()))
+  }
+
+  let b: f64;
+  match tokens[2]{
+    Token::Float(x) => b = x,
+    Token::Int(n) => b = n as f64,
+    _ => return 
+      Err(CluEError::CannotConvertToFloat(line_number, tokens[2].to_string()))
+  }
+
+  match tokens[1] {
+    Token::Plus  => Ok(Token::Float(a+b)),
+    Token::Minus => Ok(Token::Float(a-b)),
+    Token::Times => Ok(Token::Float(a*b)),
+    Token::Slash => Ok(Token::Float(a/b)),
+    _ => Err(CluEError::NotAnOperator(line_number, tokens[1].to_string())),
+  }
+
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -334,7 +463,7 @@ mod tests{
   fn test_identify_token(){
     assert_eq!(identify_token("*/").unwrap(), Token::BlockCommentEnd);
     assert_eq!(identify_token("/*").unwrap(), Token::BlockCommentStart);
-    assert_eq!(identify_token(",").unwrap(), Token::Coma);
+    assert_eq!(identify_token(",").unwrap(), Token::Comma);
     assert_eq!(identify_token("}").unwrap(), Token::CurlyBracketClose);
     assert_eq!(identify_token("{").unwrap(), Token::CurlyBracketOpen);
     assert_eq!(identify_token("element").unwrap(), Token::Element);
@@ -343,8 +472,10 @@ mod tests{
     assert_eq!(identify_token("in").unwrap(), Token::In);
     assert_eq!(identify_token("//").unwrap(), Token::LineComment);
     assert_eq!(identify_token("magnetic_field").unwrap(), Token::MagneticField);
+    assert_eq!(identify_token("-").unwrap(), Token::Minus);
     assert_eq!(identify_token(")").unwrap(), Token::ParenthesisClose);
     assert_eq!(identify_token("(").unwrap(), Token::ParenthesisOpen);
+    assert_eq!(identify_token("+").unwrap(), Token::Plus);
     assert_eq!(identify_token("residue").unwrap(), Token::Residue);
     assert_eq!(identify_token(";").unwrap(), Token::Semicolon);
     assert_eq!(identify_token("#").unwrap(), Token::Sharp);
@@ -372,13 +503,13 @@ tunnel_splitting = 80e3; // Hz"),
 
     let ref_tokens = vec![Token::Element, Token::Whitespace, Token::In,
      Token::Whitespace, Token::SquareBracketOpen, 
-     Token::UserInputValue(Box::new("H".to_string())), 
+     Token::UserInputValue("H".to_string()), 
      Token::SquareBracketClose, Token::Semicolon, Token::EOL, 
      Token::TunnelSplitting, Token::Whitespace,
      Token::Equals, Token::Whitespace, 
-     Token::UserInputValue(Box::new("80e3".to_string())), Token::Semicolon,
+     Token::UserInputValue("80e3".to_string()), Token::Semicolon,
      Token::Whitespace, Token::Slash, Token::Slash, Token::Whitespace,
-     Token::UserInputValue(Box::new("Hz".to_string())) ];
+     Token::UserInputValue("Hz".to_string()) ];
 
 
     let tokens = parse_tokens(lexer).unwrap();
@@ -403,14 +534,14 @@ tunnel_splitting = 80e3; // Hz"),
     let ref_tokens = vec![ Token::BlockCommentStart,
      Token::Element, Token::Whitespace, Token::In,
      Token::Whitespace, Token::SquareBracketOpen, 
-     Token::UserInputValue(Box::new("H".to_string())), 
+     Token::UserInputValue("H".to_string()), 
      Token::SquareBracketClose, Token::Semicolon, 
      Token::BlockCommentEnd, Token::EOL, 
      Token::TunnelSplitting, Token::Whitespace,
      Token::Equals, Token::Whitespace, 
-     Token::UserInputValue(Box::new("80e3".to_string())), Token::Semicolon,
+     Token::UserInputValue("80e3".to_string()), Token::Semicolon,
      Token::Whitespace, Token::LineComment, Token::Whitespace,
-     Token::UserInputValue(Box::new("Hz".to_string())) ];
+     Token::UserInputValue("Hz".to_string()) ];
 
     let tokens = parse_tokens(lexer).unwrap();
     let tokens = find_comments(tokens);
@@ -437,10 +568,10 @@ magnetic_field = 1.2; // T"),
     let ref_tokens = vec![ 
      Token::TunnelSplitting, 
      Token::Equals, 
-     Token::UserInputValue(Box::new("80e3".to_string())), Token::Semicolon,
+     Token::UserInputValue("80e3".to_string()), Token::Semicolon,
      Token::MagneticField, 
      Token::Equals, 
-     Token::UserInputValue(Box::new("1.2".to_string())), Token::Semicolon,
+     Token::UserInputValue("1.2".to_string()), Token::Semicolon,
     ];
 
     let tokens = parse_tokens(lexer).unwrap();
@@ -469,10 +600,10 @@ magnetic_field = 1.2; // T"),
     let ref_statements = vec![ 
      vec![Token::TunnelSplitting, 
      Token::Equals, 
-     Token::UserInputValue(Box::new("80e3".to_string()))],
+     Token::UserInputValue("80e3".to_string())],
      vec![Token::MagneticField, 
      Token::Equals, 
-     Token::UserInputValue(Box::new("1.2".to_string()))],
+     Token::UserInputValue("1.2".to_string())],
     ];
 
     let ref_line_numbers = vec![2,3];
@@ -497,6 +628,39 @@ magnetic_field = 1.2; // T"),
         assert_eq!(statements[jj][ii], ref_statements[jj][ii]);
       }
     }
+  }
+  //----------------------------------------------------------------------------
+  #[test]
+  fn test_find_lhs_rhs_delimiter_index(){
+  
+    let mut tokens = vec![Token::Float(1.0), Token::Plus, Token::Float(2.0),
+    Token::Comma, Token::Float(3.0)];
+
+    let result = find_lhs_rhs_delimiter_index(&tokens,0);
+    assert_eq!(result,Err(CluEError::NoRelationalOperators(0)));
+
+    tokens[3] = Token::Equals;
+    let result = find_lhs_rhs_delimiter_index(&tokens,0);
+    assert_eq!(result,Ok(3));
+
+    tokens[0] = Token::In;
+    let result = find_lhs_rhs_delimiter_index(&tokens,0);
+    assert_eq!(result,Err(CluEError::TooManyRelationalOperators(0)));
+  }
+  //----------------------------------------------------------------------------
+  #[allow(non_snake_case)]
+  #[test]
+  fn test_TokenExpression(){
+    let tokens = vec![Token::Float(1.0), Token::Plus, Token::Float(2.0),
+    Token::Equals, Token::Float(3.0)];
+
+    let expression = TokenExpression::from(tokens.clone(),0).unwrap();
+
+    assert_eq!(expression.relationship,tokens[3]);
+    for ii in 0..3{
+      assert_eq!(expression.lhs[ii],tokens[ii]);
+    }
+    assert_eq!(expression.rhs[0],tokens[4]);
   }
   //----------------------------------------------------------------------------
 
